282436068554102f9e4ec045385d65b3668464b7
Woochal <parkwoochal@gmail.com>
2023-12-26 15:40:24 +0900

    231226 박주영: 로딩페이지 기초 세팅

--- src/Page/Loading/index.js
+++ src/Page/Loading/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-import AppLoading from "../Components/App/App_Loading_Components/AppLoading"
-import WebLoading from "../Components/Web/Web_Loading_Components/WebLoading";
+import AppLoading from "./Components/App/AppLoading";
+import WebLoading from "./Components/Web/WebLoading";
 
 const LoadingPage = () => {
   const isDesktopOrMobile = useMediaQuery({ query: "(max-width:768px)" }); // 758px 이하일 때는 모바일 뷰로 바뀐다.
