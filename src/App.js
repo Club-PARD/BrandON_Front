1baaffef82dc6d6d81b6b3162c361956b24be291
seongju1015 <gmrfkels1015@gmail.com>
2023-12-28 17:14:21 +0900

    231228 이성주: 홈페이지 중간 저장2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  const clientId = '739066028299-o49nb40i15jpnoj2j92qtm4h89hs1r6j.apps.googleusercontent.com';
+  const clientId = process.env.REACT_APP_CLIENT_ID;
 
   return (
     <ThemeProvider theme={theme}>
