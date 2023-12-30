0528f9bff22e40451e2b907bd6bfff409dda8a17
JinseoKim <eunoia@handong.ac.kr>
2023-12-30 13:04:48 +0900

    home 충돌 해결

--- src/Page/Home/Components/Web/Webhome.js
+++ src/Page/Home/Components/Web/Webhome.js
@@ -1,12 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
-import { useRecoilState } from 'recoil';
+import { useRecoilState } from "recoil";
 import { Link } from "react-router-dom";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useGoogleLogin} from "@react-oauth/google";
-import { isLogined, accessTokenState, recoilUserID, recoilUserData, isFirstLogin } from "../../../../atom/loginAtom";
+import { useGoogleLogin } from "@react-oauth/google";
+import {
+  isLogined,
+  accessTokenState,
+  recoilUserID,
+  recoilUserData,
+  isFirstLogin,
+} from "../../../../atom/loginAtom";
 
 const WebHome = () => {
   const navigate = useNavigate();
@@ -17,80 +23,89 @@ const WebHome = () => {
   const [isFirstLoggedin, setIsFirstLoggedin] = useState();
 
   const handleLogin = (token) => {
-    localStorage.setItem('accessToken',token);
+    localStorage.setItem("accessToken", token);
     setIsLoggedIn(true);
     sendUserDataToGoogle(token);
-  }
-  
-  const sendUserDataToServer = async (userData) => { //유저의 구글정보를 서버로 보내서 디비에 저장 
-    try {
-        const jsonUserData = JSON.stringify(userData);
+  };
 
-        const response = await axios.post('http://Soim-env.eba-v9sk9m3i.ap-northeast-2.elasticbeanstalk.com/login/google', jsonUserData, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        console.log('서버 응답2:', response.data); //response.data = 유저 아이디.
-        setUserID(response.data.userId);
-        console.log('첫로그인 1',response.data.firstLogin);
-        setIsFirstLoggedin(response.data.firstLogin);
-        localStorage.setItem('userID',response.data);
-    } catch (error) {
-        console.error('서버 요청 에러2:', error);
-    }
-};
-  const sendUserDataToGoogle = async (token) => { //구글에게 억세스토큰 보내서 사용자정보 받아옴 
+  const sendUserDataToServer = async (userData) => {
+    //유저의 구글정보를 서버로 보내서 디비에 저장
     try {
-        const response = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-        console.log('서버 응답:', response.data); 
-        setUserData({
-          name: response.data.name,
-          email: response.data.email,
-          picture: response.data.picture,
-        });
-        sendUserDataToServer({
-          name: response.data.name,
-          email: response.data.email,
-          picture: response.data.picture,
-        }); // 빋은 데이터를 서버로 보내서 디비에 저장 
-    } catch (error) {
-        console.error('서버 요청 에러:', error);
-    }
-};
+      const jsonUserData = JSON.stringify(userData);
 
-  const login = useGoogleLogin({ // 구글 로그인 실행 
-    onSuccess : (res) => {
-        setAccessToken(res.access_token);
-        handleLogin(res.access_token); //억세스 토큰을 로컬스토리지에 저장하고 악시오스로 구글에게 보냄.
-        if(!isFirstLoggedin){
-          navigate('/name');
+      const response = await axios.post(
+        "http://Soim-env.eba-v9sk9m3i.ap-northeast-2.elasticbeanstalk.com/login/google",
+        jsonUserData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    },
-    onFailure : (err) => {
-        console.log(err);
+      );
+      console.log("서버 응답2:", response.data); //response.data = 유저 아이디.
+      setUserID(response.data.userId);
+      console.log("첫로그인 1", response.data.firstLogin);
+      setIsFirstLoggedin(response.data.firstLogin);
+      localStorage.setItem("userID", response.data);
+    } catch (error) {
+      console.error("서버 요청 에러2:", error);
     }
+  };
+  const sendUserDataToGoogle = async (token) => {
+    //구글에게 억세스토큰 보내서 사용자정보 받아옴
+    try {
+      const response = await axios.get(
+        "https://www.googleapis.com/oauth2/v3/userinfo",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("서버 응답:", response.data);
+      setUserData({
+        name: response.data.name,
+        email: response.data.email,
+        picture: response.data.picture,
+      });
+      sendUserDataToServer({
+        name: response.data.name,
+        email: response.data.email,
+        picture: response.data.picture,
+      }); // 빋은 데이터를 서버로 보내서 디비에 저장
+    } catch (error) {
+      console.error("서버 요청 에러:", error);
+    }
+  };
+
+  const login = useGoogleLogin({
+    // 구글 로그인 실행
+    onSuccess: (res) => {
+      setAccessToken(res.access_token);
+      handleLogin(res.access_token); //억세스 토큰을 로컬스토리지에 저장하고 악시오스로 구글에게 보냄.
+      if (!isFirstLoggedin) {
+        navigate("/name");
+      }
+    },
+    onFailure: (err) => {
+      console.log(err);
+    },
   });
 
   useEffect(() => {
     // 페이지 로드 시 로컬 스토리지에서 accessToken 확인
-    const storedToken = localStorage.getItem('accessToken');
-    console.log('첫로그인2', isFirstLoggedin)
+    const storedToken = localStorage.getItem("accessToken");
+    console.log("첫로그인2", isFirstLoggedin);
     if (storedToken) {
-      setAccessToken(storedToken); 
-      setIsLoggedIn(true); 
-      sendUserDataToGoogle(storedToken); 
+      setAccessToken(storedToken);
+      setIsLoggedIn(true);
+      sendUserDataToGoogle(storedToken);
     } else {
-      setIsLoggedIn(false); 
+      setIsLoggedIn(false);
     }
   }, []);
 
   return (
-    
     <Container>
       <HeaderText>
         <StyledText>
@@ -102,17 +117,22 @@ const WebHome = () => {
       {isLoggedIn ? (
         <TestStart>
           {isFirstLoggedin ? (
-            <LoginLink to='/name'>지금 바로 시작하기</LoginLink>
-          ): (
-            <LoginLink to='/name'>지금 바로 시작하기</LoginLink>
+            <LoginLink to="/name">지금 바로 시작하기</LoginLink>
+          ) : (
+            <LoginLink to="/name">지금 바로 시작하기</LoginLink>
           )}
         </TestStart>
-      ): (
+      ) : (
         <TestStart>
-          <button style={{all: "unset", color: "white", cursor: "pointer"}} onClick={login}>지금 바로 시작하기</button>
+          <button
+            style={{ all: "unset", color: "white", cursor: "pointer" }}
+            onClick={login}
+          >
+            지금 바로 시작하기
+          </button>
         </TestStart>
       )}
-      
+
       <OnBoading>
         <img src="Rectangle28.png"></img>
       </OnBoading>
@@ -120,14 +140,19 @@ const WebHome = () => {
       {isLoggedIn ? (
         <TestStart>
           {isFirstLoggedin ? (
-            <LoginLink to='/chat'>지금 바로 시작하기</LoginLink>
-          ): (
-            <LoginLink to='/name'>지금 바로 시작하기</LoginLink>
+            <LoginLink to="/chat">지금 바로 시작하기</LoginLink>
+          ) : (
+            <LoginLink to="/name">지금 바로 시작하기</LoginLink>
           )}
         </TestStart>
-      ): (
+      ) : (
         <TestStart>
-          <button style={{all: "unset", color: "white", cursor: "pointer"}} onClick={login}>지금 바로 시작하기</button>
+          <button
+            style={{ all: "unset", color: "white", cursor: "pointer" }}
+            onClick={login}
+          >
+            지금 바로 시작하기
+          </button>
         </TestStart>
       )}
     </Container>
